885b16c02ceae36e94336ae60f04a172c91b2c86
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-02-15 21:56:50 -0300

    Resolvendo Exercício 2 de herança do HackerRanking

---
+++ ObjectOrientedProgramming/Inheritance II/Arithmetic.cs
@@ -0,0 +1,7 @@
+namespace Inheritance_II{
+    public class Arithmetic{
+        public int Add(int a, int b){
+            return a+b;
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/Inheritance II/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Inheritance_II{     
+
+    public class Adder : Arithmetic{
+    }
+    
+    class Program{
+        static void Main(){
+            Adder a = new Adder();
+
+            Console.WriteLine("My superclass is: " + a.GetType().BaseType?.Name);
+            Console.WriteLine(a.Add(10,32) + " " + a.Add(10,3) + " " + a.Add(10,10));
+        }
+    }
+}
