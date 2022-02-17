73a9e600cf0901265697c48ee490a3ee01ca4007
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-02-17 15:16:30 -0300

    Tornando as classes do exercício 1 Internas

--- ObjectOrientedProgramming/Inheritance I/Program.cs
+++ ObjectOrientedProgramming/Inheritance I/Program.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-namespace Inheritance_I{      
+namespace Inheritance_I;
+class Animal{
+    public void Walk(){
+        Console.WriteLine("I am walking");
+    }
+}
 
-    public class Animal{
-       public void Walk(){
-           Console.WriteLine("I am walking");
-       }
+class Bird : Animal{
+    public void Fly(){
+        Console.WriteLine("I am flying");
     }
 
-    public class Bird : Animal{
-        public void Fly(){
-            Console.WriteLine("I am flying");
-        }
-
-        public void Sing(){
-            Console.WriteLine("I am singing");
-        }
+    public void Sing(){
+        Console.WriteLine("I am singing");
     }
+}
 
-    class Solution{
-        static void Main(){
-            Bird bird = new Bird();
-            bird.Walk();
-            bird.Fly();
-            bird.Sing();
-        }
+class Solution{
+    static void Main(){
+        Bird bird = new Bird();
+        bird.Walk();
+        bird.Fly();
+        bird.Sing();
     }
 }
