a172ba95a1432a8a87a9b643fcf6f9405cf713b3
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-02-21 13:27:19 -0300

    Resolvendo exercíco Iterator

---
+++ CSharp Object Oriented Programming/Iterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+class Program{
+
+    static IEnumerator Func(ArrayList myList){
+        IEnumerator it = myList.GetEnumerator();
+        while(it.MoveNext())
+            if(it.Current is string)
+                break;
+        return it;
+    }
+
+    static void Main(){
+        ArrayList myList = new ArrayList();
+
+        int n, m; 
+        int.TryParse(Console.ReadLine(), out n);
+        int.TryParse(Console.ReadLine(), out m);
+
+        for(int i = 0; i < n; i++){
+            var x=0;
+            int.TryParse(Console.ReadLine(), out x);
+            myList.Add(x);
+        }
+
+        myList.Add("###");
+
+        for(int i = 0; i < m; i++){
+            myList.Add(Console.ReadLine());
+        }
+
+        IEnumerator it = Func(myList);
+        while (it.MoveNext())
+            Console.WriteLine(it.Current.ToString());        
+    }
+}
+
+
