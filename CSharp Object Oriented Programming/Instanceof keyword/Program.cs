0850303301fe090c14ed5d296959673f8e0126cb
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-02-21 11:43:28 -0300

    Refatorando código exercício Instanceof

--- CSharp Object Oriented Programming/Instanceof keyword/Program.cs
+++ CSharp Object Oriented Programming/Instanceof keyword/Program.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 namespace InstanceOfKeyword{
     class Student{}
-    class RockStar{}
+    class Rockstar{}
     class Hacker{}
 
     class Program{
         static string Count(ArrayList mylist){
-            int a = 0,b = 0,c = 0;
+            int a = 0,b = 0,c = 0;            
             for(int i = 0; i < mylist.Count; i++){                
-                if(mylist[i].GetType() == typeof(Student))
+                if(mylist[i] is Student)
                     a++;
-                if(mylist[i].GetType() == typeof(RockStar))
+                if(mylist[i] is Rockstar)
                     b++;
-                if(mylist[i].GetType() == typeof(Hacker))
+                if(mylist[i] is Hacker)
                     c++;
             }
             String ret = a.ToString() +" "+ b.ToString() +" "+ c.ToString();
@@ -23,12 +23,14 @@ namespace InstanceOfKeyword{
 
         static void Main(){
             ArrayList myList = new ArrayList();
-            var t = int.Parse(Console.ReadLine());            
+            string? r = Console.ReadLine();
+            int t;
+            if (!int.TryParse(r, out t)) return;
             for(int i=0; i<t; i++){
                 string? s = Console.ReadLine();
-                if(!DBNull.Value.Equals(s)){
+                if(s is not null){
                     if(s.Equals("Student")) myList.Add(new Student());
-                    if(s.Equals("Rockstar")) myList.Add(new RockStar());
+                    if(s.Equals("Rockstar")) myList.Add(new Rockstar());
                     if(s.Equals("Hacker")) myList.Add(new Hacker());
                 }
             }
