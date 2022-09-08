e8b6c81c1c3df4ede5d0623645ee32570f4c6b53
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-09-07 21:05:00 -0300

    Maximum subarray sum - Kadane Algorithm

---
+++ ArrayMaxConsecutiveSum/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ArrayMaxConsecutiveSum{
+    class Program{
+        static void Main(){
+            int[] teste = new int[] {5,9,-30,15,20};            
+            Console.WriteLine(GetMaxConsecutiveSum(teste));
+        }
+
+        public static int GetMaxConsecutiveSum(int[] sourceArray){
+            int maxSum = Int32.MinValue, sum = Int32.MinValue;
+
+            for(int i=0; i < sourceArray.Length; i++){
+                if(i==0) maxSum = sum = sourceArray[i];
+                else 
+                    if(sum + sourceArray[i] > sourceArray[i])
+                        sum = sum + sourceArray[i];
+                    else sum = sourceArray[i];
+
+                if(sum > maxSum) maxSum = sum;
+            }
+            return maxSum;
+        }
+    }
+}
