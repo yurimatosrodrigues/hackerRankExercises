237c94eb166c672e43156a669081ac25d0b8d519
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-09-07 22:41:48 -0300

    Alternating Characters - Count duplicates

---
+++ AlternatingCharacters/Program.cs
@@ -0,0 +1,11 @@
+﻿int alternatingCharacters(string s)
+{
+    int deletionsSum = 0;
+    for(int i = 0; i < s.Length -1; i++){
+        if(s[i] == s[i+1])
+            deletionsSum = deletionsSum + 1;
+    }
+    return deletionsSum;
+}
+
+Console.WriteLine(alternatingCharacters("AAABBB"));
